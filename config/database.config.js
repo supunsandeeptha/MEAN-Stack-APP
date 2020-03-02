afc960216b0b9c9a74fa8da2947e4a1f0e7df78f
supunsandeeptha <sandeeptha.supun@gmail.com>
2020-03-02 12:02:26 +1100

    setting up the database connection

---
+++ config/database.config.js
@@ -0,0 +1,6 @@
+// database connection string
+const databseconnection = 'mongodb://localhost:27017/tutorial';
+
+module.exports = {
+    url: databseconnection
+}
--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const dbConfig = require('./config/database.config.js');
+
 // creating the express app
 const app = express();
 
+//connecting to the database
+mongoose.connect(dbConfig.url, {
+
+}).then(() => {
+    console.log("successfully connected to the database");
+}).catch(err => {
+    console.log("Could not connect to the database");
+});
+
+//using the middleware morgan
+app.use(morgan('dev'));
 
 //craeting a simple request
 app.get('/', function (req, res) {
