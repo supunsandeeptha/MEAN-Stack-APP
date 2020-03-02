3a041e7bb5feb728393263f66ce4b22487839e6d
supunsandeeptha <sandeeptha.supun@gmail.com>
2020-03-02 15:03:52 +1100

    adding validations for the add user route

--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 const MongSchema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 var UserSchema = new MongSchema({
     username: { type: String, lowercase: true, required: true, unique: true },
     password: { type: String, required: true },
     email: { type: String, required: true, lowercase: true, unique: true }
+});
+
+UserSchema.pre('save', function (next) {
+    const user = this;
+    bcrypt.hash(user.password, null, null, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+    });
 });
 
 module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const dbConfig = require('./config/database.config.js');
+//importing the user model
+const User = require('./app/models/user.js');
+// require body-parser
+const bodyParser = require('body-parser');
 
 // creating the express app
 const app = express();
+
+//for parsing application/Json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //connecting to the database
 mongoose.connect(dbConfig.url, {
@@ -21,6 +29,26 @@ app.use(morgan('dev'));
 //craeting a simple request
 app.get('/', function (req, res) {
     res.send('Hello World');
+});
+
+// route to create users
+app.post('/users', function (req, res) {
+    //creating a new user
+    const user = new User();
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.email = req.body.email;
+    if (req.body.username == null || req.body.username == "" || req.body.password == null || req.body.password == "" || req.body.email == null || req.body.email == "") {
+        res.send('Please check your username, password or e-mail');
+    } else {
+        user.save(function (err) {
+            if (err) {
+                res.send('Username or E-mail already exists');
+            } else {
+                res.send('User created !');
+            }
+        });
+    }
 });
 
 app.listen(8080, function () {
