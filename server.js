c6fc1dc1c85698e11939e95838f5b07a9a275e64
supunsandeeptha <sandeeptha.supun@gmail.com>
2020-03-18 10:12:28 +1100

    enabling to view the static html file from server js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dbConfig = require('./config/database.config.js');
 const bodyParser = require('body-parser');
 const router = express.Router();
 const appRoutes = require('./app/routes/routes.js')(router);
+const path = require('path');
 
 // creating the express app
 const app = express();
@@ -15,6 +16,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //using the router
 app.use('/api', appRoutes);
+//making the public folder available to access 
+app.use(express.static(__dirname + '/public'));
+// injecting the html file
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+});
 
 //connecting to the database
 mongoose.connect(dbConfig.url, {
